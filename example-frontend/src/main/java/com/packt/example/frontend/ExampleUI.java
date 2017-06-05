2c7b833b3fad4dfd2a40df4b06751a509735c9a9
Peter Lehto <peter@vaadin.com>
2017-06-05 21:05:57 +0300

    Initial distribution s2-t1-3

---
+++ example-frontend/src/main/java/com/packt/example/frontend/ExampleUI.java
@@ -0,0 +1,23 @@
+package com.packt.example.frontend;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.UI;
+
+public class ExampleUI extends UI {
+
+	@Override
+	protected void init(VaadinRequest request) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@WebServlet(urlPatterns = "/*", name = "VaadinServlet", asyncSupported = true)
+	@VaadinServletConfiguration(ui = ExampleUI.class, productionMode = false)
+	public static class MyUIServlet extends VaadinServlet {
+	}
+
+}
