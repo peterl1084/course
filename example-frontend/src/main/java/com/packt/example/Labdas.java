c08978daaeadce91ab37b2c78aa4ba5d49b01b55
peter <peter@vaadin.com>
2017-08-08 23:23:35 +0300

    Lambda example of persons

---
+++ example-frontend/src/main/java/com/packt/example/Labdas.java
@@ -0,0 +1,40 @@
+package com.packt.example;
+
+import java.util.Arrays;
+import java.util.function.Predicate;
+
+public class Labdas {
+
+	public static void main(String... args) {
+		printPersons(p -> p.getAge() >= 18, p("a", 17), p("b", 21));
+	}
+
+	public static void printPersons(Predicate<Person> predicate,
+			Person... persons) {
+		Arrays.asList(persons).stream().filter(predicate)
+				.map(p -> p.getName() + " " + p.getAge())
+				.forEach(System.out::println);
+	}
+
+	public static Person p(String name, int age) {
+		return new Person(name, age);
+	}
+
+	private static class Person {
+		private String name;
+		private int age;
+
+		public Person(String name, int age) {
+			this.name = name;
+			this.age = age;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public int getAge() {
+			return age;
+		}
+	}
+}
